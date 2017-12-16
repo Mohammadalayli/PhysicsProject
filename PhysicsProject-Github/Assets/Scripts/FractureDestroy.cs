07b0b3f92f48d8df15598a915d34c2bd22a90cb8
Mohammadalayli <34610210+Mohammadalayli@users.noreply.github.com>
2017-12-17 01:38:40 +0200

    Cube now fracture on impact

---
+++ PhysicsProject-Github/Assets/Scripts/FractureDestroy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FractureDestroy : MonoBehaviour {
+
+    public GameObject cracked;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+            Instantiate(cracked, transform.position, transform.rotation);
+        }
+    }
+    
+}
